a4e65e4153d43e6d4a1e57f0c624eb172ae5e246
Nabeel Ali Memon <nabeel.memon@intigral.net>
2022-05-01 06:11:01 +0400

    Added tests to milestone_1

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ enum Card {
 
 #[derive(Clone, PartialEq)]
 struct Deck {
-	cards: Vec<Card>
+	cards: Vec<Card>,
 }
 
 struct Alice {
@@ -70,5 +70,118 @@ impl Deck {
 			cards if cards[1] == Card::Queen || cards[4] == Card::Queen => true,
 			_ => false
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use crate::Card::{King, Queen};
+	use crate::{Alice, Bob, Deck};
+
+	#[test]
+	fn test_cyclic_shift() {
+		let mut deck = Deck {
+			cards: vec![King, King, King, King, Queen]
+		};
+		deck.cycle_shift(2);
+		assert_eq!(deck.cards, vec![King, King, Queen, King, King]);
+		deck.cycle_shift(3);
+		assert_eq!(deck.cards, vec![King, King, King, King, Queen]);
+		assert_ne!(deck.cards, vec![King, Queen, King, King, King]);
+	}
+
+	#[test]
+	fn test_full_game_var_shifts() {
+		let alice = Alice { secret_input: true };
+		let bob = Bob { secret_input: true };
+		let alice_deck = alice.encode_alice_input();
+		let bob_deck = bob.encode_bob_input();
+
+		let mut deck1 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck2 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck3 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck4 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck5 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+
+		deck1.cycle_shift(1);
+		deck2.cycle_shift(2);
+		deck3.cycle_shift(3);
+		deck4.cycle_shift(4);
+		deck5.cycle_shift(5);
+
+		assert_eq!(deck1.decode(), true);
+		assert_eq!(deck2.decode(), true);
+		assert_eq!(deck3.decode(), true);
+		assert_eq!(deck4.decode(), true);
+		assert_eq!(deck5.decode(), true);
+
+		let alice = Alice { secret_input: true };
+		let bob = Bob { secret_input: false, };
+
+		let alice_deck = alice.encode_alice_input();
+		let bob_deck = bob.encode_bob_input();
+		let mut deck1 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck2 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck3 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck4 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck5 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+
+		deck1.cycle_shift(1);
+		deck2.cycle_shift(2);
+		deck3.cycle_shift(3);
+		deck4.cycle_shift(4);
+		deck5.cycle_shift(5);
+
+		assert_eq!(deck1.decode(), false);
+		assert_eq!(deck2.decode(), false);
+		assert_eq!(deck3.decode(), false);
+		assert_eq!(deck4.decode(), false);
+		assert_eq!(deck5.decode(), false);
+
+		let alice = Alice { secret_input: false };
+		let bob = Bob { secret_input: true, };
+
+		let alice_deck = alice.encode_alice_input();
+		let bob_deck = bob.encode_bob_input();
+		let mut deck1 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck2 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck3 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck4 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck5 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+
+		deck1.cycle_shift(1);
+		deck2.cycle_shift(2);
+		deck3.cycle_shift(3);
+		deck4.cycle_shift(4);
+		deck5.cycle_shift(5);
+
+		assert_eq!(deck1.decode(), false);
+		assert_eq!(deck2.decode(), false);
+		assert_eq!(deck3.decode(), false);
+		assert_eq!(deck4.decode(), false);
+		assert_eq!(deck5.decode(), false);
+
+		let alice = Alice { secret_input: false };
+		let bob = Bob { secret_input: false, };
+
+		let alice_deck = alice.encode_alice_input();
+		let bob_deck = bob.encode_bob_input();
+		let mut deck1 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck2 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck3 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck4 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+		let mut deck5 = Deck::join_decks(alice_deck.clone(), bob_deck.clone());
+
+		deck1.cycle_shift(1);
+		deck2.cycle_shift(2);
+		deck3.cycle_shift(3);
+		deck4.cycle_shift(4);
+		deck5.cycle_shift(5);
+
+		assert_eq!(deck1.decode(), false);
+		assert_eq!(deck2.decode(), false);
+		assert_eq!(deck3.decode(), false);
+		assert_eq!(deck4.decode(), false);
+		assert_eq!(deck5.decode(), false);
 	}
 }
